46f883215c2281dcc238cfdadf1e659b6dd7108a
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-01-17 02:31:27 +0530

    /

--- live contest/Long challenge/CRYCOLR.cpp
+++ live contest/Long challenge/CRYCOLR.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 
-signed main()
+int main()
 {
     int t;
     cin >> t;
@@ -10,36 +9,19 @@ signed main()
     {
         int n;
         cin >> n;
-        int R[3], G[3], B[3], r, g, b, result, zero = 0;
+        int R[3], G[3], B[3], r, g, b;
         for (int i = 0; i < 3; i++)
         {
             cin >> R[i];
-            if (R[i] == 0)
-            {
-                zero++;
-            }
         }
         for (int i = 0; i < 3; i++)
         {
             cin >> G[i];
-            if (G[i] == 0)
-            {
-                zero++;
-            }
         }
         for (int i = 0; i < 3; i++)
         {
             cin >> B[i];
-            if (B[i] == 0)
-            {
-                zero++;
-            }
         }
-        r = n - R[0];
-        g = n - G[1];
-        b = n - B[2];
-        result = max(r, g);
-        result = max(result, b);
     }
     return 0;
 }
