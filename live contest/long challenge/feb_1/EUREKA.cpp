553a52d13008aa20595fe821dca861cafe26d2bd
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-02-04 18:04:02 +0530

    </>

--- live contest/long challenge/feb_1/EUREKA.cpp
+++ live contest/long challenge/feb_1/EUREKA.cpp
@@ -1,13 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
+#define int long long
+signed main()
 {
 
     int test;
     cin >> test;
     while (test--)
     {
+        int n;
+        cin >> n;
+
+        double d = pow((0.143 * n), n);
+        int a = floor(d);
+        if ((d - a) < 0.5)
+        {
+            cout << a << endl;
+        }
+        else
+        {
+            cout << a + 1 << endl;
+        }
     }
     return 0;
 }
