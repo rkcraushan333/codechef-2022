38634cb845264d5a533db165e193c3f6c42b1b51
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-02-04 17:31:31 +0530

    </>

---
+++ live contest/long challenge/feb_1/CHEFBARBER.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int naua, m;
+        cin >> naua >> m;
+        cout << naua * m << endl;
+    }
+    return 0;
+}
